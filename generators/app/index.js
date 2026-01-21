eb6a80cc19122f82792c7c22b72e502edab1315e
Rui Fernandes <ruipedro16@protonmail.com>
2026-01-21 19:21:16 +0000

    remove dummyfile stuff

--- generators/app/index.js
+++ generators/app/index.js
@@ -73,11 +73,6 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
-    );
-
     this.fs.copyTpl(
       this.templatePath("preamble.tex"),
       this.destinationPath("preamble.tex"),
